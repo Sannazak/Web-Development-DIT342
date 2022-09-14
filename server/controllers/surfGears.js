cac219bc3b1d72331edc08542782155bc91317a8
Patricia Marklund <antunes@student.chalmers.se>
2022-09-14 10:39:19 +0100

    Patch and Put for surfGear and surfSpot

--- server/controllers/surfGears.js
+++ server/controllers/surfGears.js
@@ -50,5 +50,38 @@ router.delete('/api/surfGears/:id', function(req, res, next){
     });
 });
 
+router.put('/api/surfGears/:id', function(req, res, next) {
+    var id = req.params.id;
+    SurfGear.findById(id, function(err, surfGear) {
+        if (err) { return next(err); }
+        if (surfGear == null) {
+            return res.status(404).json({"message": "Surf Gear not found"});
+        }
+        surfGear.name = req.body.name;
+        surfGear.price = req.body.price;
+        surfGear.size = req.body.size;
+        surfGear.gender = req.body.gender;
+        surfGear.description = req.body.description;
+        surfGear.save();
+        res.json(surfGear);
+    });
+});
+
+router.patch('/api/surfGears/:id', function(req, res, next) {
+    var id = req.params.id;
+    SurfGear.findById(id, function(err, surfGear) {
+        if (err) { return next(err); }
+        if (surfGear == null) {
+            return res.status(404).json({"message": "Surf Gear not found"});
+        }
+        surfGear.name = (req.body.name || surfGear.name);
+        surfGear.price = (req.body.price || surfGear.price);
+        surfGear.size = (req.body.size || surfGear.size);
+        surfGear.gender = (req.body.gender || surfGear.gender);
+        surfGear.description = (req.body.description || surfGear.description);
+        surfGear.save();
+        res.json(surfGear);
+    });
+});
 
 module.exports = router;
