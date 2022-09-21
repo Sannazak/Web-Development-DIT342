ad1b083ad421c9c861e1794e265a7941231c07dd
Patricia Marklund <antunes@student.chalmers.se>
2022-09-21 11:38:00 +0200

    updates on controllers

--- server/controllers/surfBoards.js
+++ server/controllers/surfBoards.js
@@ -1,73 +1,78 @@
 var express = require('express');
-const surfBoards = require('../models/surfBoards');
 var router = express.Router();
 var SurfBoards = require('../models/surfBoards');
 
+//Create a new surf board
 router.post('/api/surfBoards', function(req, res, next){
     var surfBoards = new SurfBoards(req.body);
     surfBoards.save(function(err, surfBoards){
-        if(err) {return next(err); }
+        if(err) { return res.status(400); }
         res.status(201).json(surfBoards);
     });
 });
 
+//Get all the surf boards
 router.get('/api/surfBoards', function(req, res, next){
     SurfBoards.find(function(err, surfBoards){
-        if(err) {return next(err);}
-        res.json({'surfBoards': surfBoards})
+        if(err) { return res.status(400);}
+        res.status(200).json(surfBoards);
     });
 });
 
+//Delete all the surf boards 
 router.delete('/api/surfBoards', function(req, res, next){
     SurfBoards.deleteMany(function(err, surfBoards){
         if(err) {return next(err);}
         if(surfBoards === null){
-            return res.status(404).json({'message': 'Surf Board not found'});
+            return res.status(404).json({'message': 'Surfboard not found'});
         }
-        res.json(surfBoards);
+        res.status(202).json(surfBoards);
     });
 });
 
+//Get a surf board by its ID
 router.get('/api/surfBoards/:id', function(req, res, next){
     var id = req.params.id;
     SurfBoards.findById(id, function(err, surfBoards){
         if(err) {return next(err);}
         if(surfBoards === null){
-            return res.status(404).json({'message': 'Surf Board not found'});
+            return res.status(404).json({'message': 'Surfboard not found'});
         }
-        res.json(surfBoards);
+        res.status(200).json(surfBoards);
     });
 });
 
+//Delete a surfboard by its ID
 router.delete('/api/surfBoards/:id', function(req, res, next){
     var id = req.params.id;
     SurfBoards.findOneAndDelete({_id: id}, function(err, surfBoards){
         if(err) {return next(err);}
         if(surfBoards === null){
-            return res.status(404).json({'message': 'Surf Board not found'});
+            return res.status(404).json({'message': 'Surfboard not found'});
         }
-        res.json(surfBoards);
+        res.status(202).json(surfBoards);
     });
 });
 
 router.put('/api/surfBoards/:id', function(req, res, next) {
+    //TO DO: make the code shorter
     var id = req.params.id;
     SurfBoards.findById(id, function(err, surfBoards) {
         if (err) { return next(err); }
         if (surfBoards == null) {
             return res.status(404).json({"message": "Surf Board not found"});
         }
-        
         surfBoards.price = req.body.price;
         surfBoards.size = req.body.size;
         surfBoards.volume = req.body.volume;
         surfBoards.style = req.body.style;
         surfBoards.save();
-        res.json(surfBoards);
+        res.status(201).json(surfBoards);
     });
 });
 
 router.patch('/api/surfBoards/:id', function(req, res, next) {
+    //TO DO: make the code shorter
     var id = req.params.id;
     SurfBoards.findById(id, function(err, surfBoards) {
         if (err) { return next(err); }
@@ -79,7 +84,7 @@ router.patch('/api/surfBoards/:id', function(req, res, next) {
         surfBoards.volume = (req.body.volume || surfBoards.volume);
         surfBoards.style = (req.body.style || surfBoards.style);
         surfBoards.save();
-        res.json(surfBoards);
+        res.status(201).json(surfBoards);
     });
 });
 
--- server/controllers/surfGears.js
+++ server/controllers/surfGears.js
@@ -1,9 +1,10 @@
 var express = require('express');
-const surfGear = require('../models/surfGear');
 var router = express.Router();
 var SurfGear = require('../models/surfGear');
 
+//Create a new surf gear
 router.post('/api/surfGears', function(req, res, next){
+    //TO DO: make a check for the required attribute
     var surfGear = new SurfGear(req.body);
     surfGear.save(function(err, surfGear){
         if(err) {return next(err); }
@@ -11,23 +12,26 @@ router.post('/api/surfGears', function(req, res, next){
     });
 });
 
+//Get all the surf gears
 router.get('/api/surfGears', function(req, res, next){
     SurfGear.find(function(err, surfGears){
         if(err) {return next(err);}
-        res.json({'surfGears': surfGears})
+        res.status(200).json(surfGears);
     });
 });
 
+//Delete all the surf gears
 router.delete('/api/surfGears', function(req, res, next){
-    SurfGear.deleteMany(function(err, surfGears){
+    SurfGear.deleteMany(function(err, surfGear){
         if(err) {return next(err);}
         if(surfGear === null){
             return res.status(404).json({'message': 'Surf Gear not found'});
         }
-        res.json(surfGear);
+        res.status(202).json(surfGear);
     });
 });
 
+//Get a surf gear by it's ID
 router.get('/api/surfGears/:id', function(req, res, next){
     var id = req.params.id;
     SurfGear.findById(id, function(err, surfGear){
@@ -35,10 +39,11 @@ router.get('/api/surfGears/:id', function(req, res, next){
         if(surfGear === null){
             return res.status(404).json({'message': 'Surf Gear not found'});
         }
-        res.json(surfGear);
+        res.status(200).json(surfGear);
     });
 });
 
+//Delete a surf gear by its ID
 router.delete('/api/surfGears/:id', function(req, res, next){
     var id = req.params.id;
     SurfGear.findOneAndDelete({_id: id}, function(err, surfGear){
@@ -46,11 +51,12 @@ router.delete('/api/surfGears/:id', function(req, res, next){
         if(surfGear === null){
             return res.status(404).json({'message': 'Surf Gear not found'});
         }
-        res.json(surfGear);
+        res.status(202).json(surfGear);
     });
 });
 
 router.put('/api/surfGears/:id', function(req, res, next) {
+    //TO DO: make the code shorter
     var id = req.params.id;
     SurfGear.findById(id, function(err, surfGear) {
         if (err) { return next(err); }
@@ -63,11 +69,12 @@ router.put('/api/surfGears/:id', function(req, res, next) {
         surfGear.gender = req.body.gender;
         surfGear.description = req.body.description;
         surfGear.save();
-        res.json(surfGear);
+        res.status(201).json(surfGear);
     });
 });
 
 router.patch('/api/surfGears/:id', function(req, res, next) {
+    //TO DO: make the code shorter
     var id = req.params.id;
     SurfGear.findById(id, function(err, surfGear) {
         if (err) { return next(err); }
@@ -80,7 +87,7 @@ router.patch('/api/surfGears/:id', function(req, res, next) {
         surfGear.gender = (req.body.gender || surfGear.gender);
         surfGear.description = (req.body.description || surfGear.description);
         surfGear.save();
-        res.json(surfGear);
+        res.status(201).json(surfGear);
     });
 });
 
