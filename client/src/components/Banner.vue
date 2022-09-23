f04afd22a9469fdb61d3d26247eb6baa687e700b
-Sanna <sannaza@student.chalmers.se>
2022-09-23 22:03:39 +0200

    added text above picture in banner

--- client/src/components/Banner.vue
+++ client/src/components/Banner.vue
@@ -1,9 +1,15 @@
 <template>
-    <div>
+    <div class="container">
         <img class="image"
             src="../assets/austin-neill-uHD0uyp79Dg-unsplash4.jpg"
             alt="Surf Photo"
         />
+        <div class="text-block">
+            <h6>Welcome!</h6>
+            <p>The days of carrying heavy surfbags and gears through airports is over.
+                Find all the stores, boards, gears and lessons on your next surftrip to keep the stoke!
+            </p>
+        </div>
     </div>
 </template>
 
@@ -12,14 +18,27 @@ export default { name: 'banner' }
 </script>
 
 <style scoped>
+.container {
+  position:relative;
+  padding-left: 1px;
+  padding-top: 1px;
+}
 .image {
-    width: 1300px;
+    width: 1150px;
     background-size: cover;
     object-fit:scale-down;
 }
 
-h1 {
-    color:blue;
+.text-block {
+  position: absolute;
+  bottom: 20px;
+  left: 60px;
+  right: 900px;
+  font-size: x-small;
+  background-color: rgba(0, 0, 0, 0.478);
+  color: white;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 10px;
 }
-
 </style>
