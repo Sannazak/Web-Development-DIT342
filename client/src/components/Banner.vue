3050e2463efe4f8c58d2c154045d165925b33d26
-Sanna <sannaza@student.chalmers.se>
2022-09-23 20:39:07 +0200

    added picture to the banner

---
+++ client/src/components/Banner.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <img class="image"
+            src="../assets/austin-neill-uHD0uyp79Dg-unsplash.jpg"
+            alt="Surf Photo"
+        />
+    </div>
+</template>
+
+<script>
+export default { name: 'banner' }
+</script>
+
+<style scoped>
+.image {
+    width: 100%;
+    height: -15%;
+    background-size: cover;
+}
+
+h1 {
+    color:blue;
+}
+
+</style>
