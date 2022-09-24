24aa14465662b40a0105b3fe045ce24345b2a207
-Sanna <sannaza@student.chalmers.se>
2022-09-24 14:54:18 +0200

    added picture search png and view page

---
+++ client/src/views/SearchResult.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <banner />
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Banner from '@/components/Banner.vue'
+
+export default {
+  name: 'search-result',
+  components: { Banner },
+  data() {
+    return {
+      message: 'none'
+    }
+  }
+} </script>
+<style>
+
+</style>
