896c9c6244820483487822528b1cf5479bff9c67
Patricia Marklund <antunes@student.chalmers.se>
2022-09-26 21:07:53 +0200

    User registration page

---
+++ client/src/views/User.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>Text Input</h2>
+        <input v-model="text"> {{ text }}
+        <h2>Select</h2>
+        <select v-model="selected">
+            <option disabled value="">Please select one</option>
+            <option>A</option>
+            <option>B</option>
+            <option>C</option>
+        </select>
+        <span>Selected: {{ selected }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: 'Edit me',
+      selected: 'A'
+    }
+  }
+}
+</script>
