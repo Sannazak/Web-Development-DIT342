706dc39a2d5212ee7f578ea5d864b937a27b4a99
-Sanna <sannaza@student.chalmers.se>
2022-09-30 11:12:23 +0200

    go from searchresult to searchview and deck the cards

--- client/src/views/SearchResult.vue
+++ client/src/views/SearchResult.vue
@@ -1,28 +1,34 @@
 <template>
   <div>
-    <banner/>
-    <FormSelect/>
+    <banner />
+    <FormSelect />
     <!-- <storeItem v-bind:store="store"/>   v-on:get-store="getStore" -->
-    <div v-for="stores in this.store" v-bind:key="stores.name">
-  <div>
-      <b-card
-      img-src="https://picsum.photos/600/300/?image=25"
-      img-alt="Image"
-      img-top
-      tag="article"
-      style="max-width: 20rem;, max-height: 50px"
-      class="mb-2"
-    >
-    <b-card-title><h2>{{stores.name}}</h2></b-card-title>
+    <b-card-group deck>
+      <div v-for="stores in this.store" v-bind:key="stores.name">
+        <b-card
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 20rem"
+          class="mb-2"
+        >
+          <b-card-title
+            ><h2>{{ stores.name }}</h2></b-card-title
+          >
 
-      <b-card-text>
-        {{stores.name}} <br> {{stores._id}} <br> {{stores.adress.country}}
-      </b-card-text>
+          <b-card-text>
+            {{ stores.name }} <br />
+            {{ stores._id }} <br />
+            {{ stores.adress.country }}
+          </b-card-text>
 
-      <b-button variant="primary" v-on:click= OnClick(stores._id) >Visit Store</b-button>
-    </b-card>
-  </div>
-</div>
+          <b-button variant="primary" v-on:click="OnClick(stores._id)"
+            >Visit Store</b-button
+          >
+        </b-card>
+      </div>
+    </b-card-group>
   </div>
 </template>
 
@@ -38,43 +44,46 @@ export default {
   mounted() {
     console.log('page is loaded')
     Api.get('/stores')
-      .then(response => {
+      .then((response) => {
         console.log(response.data)
         this.store = response.data
         console.log('api saved')
         console.log(this.store)
         console.log(this.store.name) // console.log(response.data)
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error)
       })
   },
   data() {
     return {
-      store: [{
-        _id: '',
-        name: '',
-        adress: {
-          country: '',
-          street: '',
-          streetNr: '',
-          postalCode: '',
-          city: ''
-        },
-        surfBoards: [],
-        surfLessons: [],
-        surfGears: []
-      }]
+      store: [
+        {
+          _id: '',
+          name: '',
+          adress: {
+            country: '',
+            street: '',
+            streetNr: '',
+            postalCode: '',
+            city: ''
+          },
+          surfBoards: [],
+          surfLessons: [],
+          surfGears: []
+        }
+      ]
     }
   },
   methods: {
-    OnClick(id) { // change to show one store
+    OnClick(id) {
+      // change to show one store
       console.log(id)
+      window.location = 'StoreView/' + id
     }
   }
 }
 </script>
 
 <style>
-
 </style>
