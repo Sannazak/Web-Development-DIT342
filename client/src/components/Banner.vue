4a02fa3c533b2dfd7354319d7046ebe56cb3dc00
-Sanna <sannaza@student.chalmers.se>
2022-10-04 11:36:06 +0200

    deletes namefield search, adds to show all stores if no store is selected

--- client/src/components/Banner.vue
+++ client/src/components/Banner.vue
@@ -1,20 +1,21 @@
 <template>
-    <div>
-        <img class="image"
-            src="../assets/banner_photo.jpg"
-            alt="Surf Photo"
-        />
-        <!-- <div class="text-block">
-            <h6>Welcome!</h6>
-            <p>The days of carrying heavy surfbags and gears through airports is over.
-                Find all the stores, boards, gears and lessons on your next surftrip and keep the stoke!
-            </p>
-        </div>
-        <form action="https://www.google.com" method="get" class="search-bar">
-            <input type="text" placeholder="Search for location or shop" name="q">
-            <button type="submit" @click=getMessage()><img src="../assets/search.png"></button>
-        </form> -->
+  <div class="container">
+    <img class="image" src="../assets/banner_photo.jpg" alt="Surf Photo" />
+    <div class="text-block">
+      <h6>Welcome!</h6>
+      <p>
+        The days of carrying heavy surfbags and gears through airports is over.
+        Find all the stores, boards, gears and lessons on your next surftrip and
+        keep the stoke!
+      </p>
     </div>
+    <form action="https://www.google.com" method="get" class="search-bar">
+      <input type="text" placeholder="Search for location or shop" name="q" />
+      <button type="submit" @click="getMessage()">
+        <img src="../assets/search.png" />
+      </button>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -30,10 +31,10 @@ export default {
   methods: {
     getMessage() {
       Api.get('/')
-        .then(response => {
+        .then((response) => {
           this.message = response.data.message
         })
-        .catch(error => {
+        .catch((error) => {
           this.message = error
         })
     }
@@ -43,58 +44,58 @@ export default {
 
 <style scoped>
 .container {
-  /* position: relative;
+  position: relative;
   padding-left: 1px;
   padding-top: 1px;
   display: flex;
   align-items: right;
-  justify-content: right; */
+  justify-content: right;
 }
-.search-bar{
-    width: 100%;
-    max-width: 250px;
-    background: rgba(255, 255, 255, 0.2);
-    display:flex;
-    position: absolute;
-    border-radius: 60px;
-    bottom:8px;
-    right:28px;
-    padding: 5px 10px;
-    backdrop-filter: blur(1px);
-}
-
-.search-bar input{
-    background: transparent;
-    flex: 1;
-    border: 0;
-    outline: none;
-    padding: 6px 5px;
-    font-size: 15px;
-    color:#f7f9f9;
+.search-bar {
+  width: 100%;
+  max-width: 250px;
+  background: rgba(255, 255, 255, 0.2);
+  display: flex;
+  position: absolute;
+  border-radius: 60px;
+  bottom: 8px;
+  right: 28px;
+  padding: 5px 10px;
+  backdrop-filter: blur(1px);
 }
 
-::placeholder{
-    color:#f7f9f9;
+.search-bar input {
+  background: transparent;
+  flex: 1;
+  border: 0;
+  outline: none;
+  padding: 6px 5px;
+  font-size: 15px;
+  color: #f7f9f9;
 }
 
-.search-bar button img{
-    width:15px;
+::placeholder {
+  color: #f7f9f9;
 }
 
-.search-bar button{
-    border: 0;
-    border-radius: 50%;
-    width: 30px;
-    height: 30px;
-    background: #054e47;
-    cursor: pointer;
+.search-bar button img {
+  width: 15px;
+}
+
+.search-bar button {
+  border: 0;
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  background: #054e47;
+  cursor: pointer;
 }
 .image {
-    width: 100%;
-    background-size: cover;
-    object-fit:scale-down;
-    min-height: 100%;
-    bottom: 0%;
+  width: 100%;
+  background-size: cover;
+  object-fit: scale-down;
+  min-height: 100%;
+  bottom: 0%;
 }
 
 .text-block {
--- client/src/views/SearchResult.vue
+++ client/src/views/SearchResult.vue
@@ -2,43 +2,32 @@
   <div>
     <banner />
     <div class="container-big">
-      <b-form-input v-model="text" placeholder="Search for a specific store" :formatter="formatter"></b-form-input>
-      <b-form-select v-model="selected" :options="options" ></b-form-select>
-      <div class="mt-3">Stores close to: <strong>{{ selected }}</strong></div>
-    <!-- <FormSelect @selectedCity="getStoreBySelectedCity"/> // get from formselect
-    <h1>{{ selectedCity }}</h1> -->
-    <!-- <storeItem v-bind:store="store"/>   v-on:get-store="getStore" -->
-    <b-card-group deck id="deck-cards">
-      <div v-for="stores in store" v-bind:key="stores._id">
-        <div v-if="text === stores.name || selected === stores.adress.city">
-        <!-- <div class=”matchedStores” v-for="stores in matchedStores" v-bind:key="stores._id"> -->
-        <b-card
-          overlay
-          img-src="https://i.picsum.photos/id/1041/200/250.jpg?hmac=GACWtMArqhQoRRs9_QZTU0B7l5Z2vbZFUdg0ZoFoVcM"
-          img-alt="Card Image"
-          text-variant="white"
-          tag="article"
-          style="max-width: 50rem;"
-          class="mb-2"
-        >
-          <b-card-title
-            ><h2>{{ stores.name }}</h2></b-card-title
-          >
-
-          <b-card-text>
-            {{ stores.adress.city }} <br />
-            {{ stores.adress.postalCode }} <br />
-            {{ stores.adress.country }}
-          </b-card-text>
-
-          <b-button variant="dark" v-on:click="OnClick(stores._id)"
-            >Visit Store</b-button
-          >
-        </b-card>
-        </div>
+      <b-form-select
+        v-model="selected"
+        @change="onSelectedCity(selected)"
+        :options="options"
+      ></b-form-select>
+      <div class="mt-3">
+        Stores close to: <strong>{{ selected }}</strong>
       </div>
-    </b-card-group>
-  </div>
+      <b-card-group deck id="deck-cards">
+        <div v-for="stores in store" v-bind:key="stores._id">
+          <b-card id="resultCards" tag="article"  >
+            <b-card-title>
+              <h2>{{ stores.name }}</h2>
+            </b-card-title>
+            <b-card-text>
+              {{ stores.adress.city }} <br />
+              {{ stores.adress.postalCode }} <br />
+              {{ stores.adress.country }}
+            </b-card-text>
+            <b-button variant="dark" v-on:click="OnClick(stores._id)">
+              Visit Store
+            </b-button>
+          </b-card>
+        </div>
+      </b-card-group>
+    </div>
   </div>
 </template>
 
@@ -50,20 +39,10 @@ import { Api } from '@/Api'
 
 export default {
   name: 'searchResult',
-  components: { Banner/* , FormSelect */ },
+  components: { Banner /* , FormSelect */ },
   mounted() {
     console.log('page is loaded')
-    Api.get('/stores')
-      .then((response) => {
-        console.log(response.data)
-        this.store = response.data
-        console.log('api saved')
-        console.log(this.store)
-        console.log(this.store.name) // console.log(response.data)
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+    this.getAllStores()
   },
   data() {
     return {
@@ -93,41 +72,69 @@ export default {
           surfLessons: [],
           surfGears: []
         }
-      ],
-      /* selectedCity: '', */
-      /* computed: {
-        matchedStores: function () {
-          // eslint-disable-next-line no-undef
-          return _.pickBy(this.stores, function (s) {
-            if (s.city === this.selected.value) { return s.city }
-          })
-        }
-      }, */
-      methods: {
-        OnClick(id) {
-        // change view to show one store
-          console.log(id)
-          window.location = 'StoreView/' + id
-        },
-        formatter(value) {
-          return value.toUpperCase()
-        }/* ,
-        getStoreBySelectedCity(value) {
-          this.selectedCity = value
-        } */
+      ]
+    }
+  },
+  methods: {
+    getAllStores() {
+      Api.get('/stores/')
+        .then((response) => {
+          console.log(response.data)
+          this.store = response.data
+          console.log('api saved')
+          console.log(this.store)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+        .then(() => {
+          // always executes
+        })
+    },
+    onSelectedCity(selected) {
+      if (selected === null) {
+        this.getAllStores()
+      } else {
+        this.getFilteredStores(selected)
       }
+    },
+    OnClick(id) {
+      // change view to show one store
+      console.log(id)
+      window.location = 'StoreView/' + id
+    },
+    formatter(value) {
+      return value.toUpperCase()
+    },
+    getFilteredStores(selected) {
+      Api.get('/stores/?adress.city=' + selected)
+        .then((response) => {
+          this.store = response.data
+          console.log(selected)
+          console.log(this.store)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
     }
   }
 }
 </script>
 
 <style>
-  .container-big {
-    max-width: 1150px;
-    margin: 0 auto;
-  }
-  .deck-cards {
-    margin: 0 auto;
-    align-content: center;
-  }
+.container-big {
+  max-width: 1150px;
+  margin: 0 auto;
+  margin-top: 25px;
+}
+.deck-cards {
+  align-items: center;
+}
+#resultCards {
+  background-color: #054e47af;
+  color: white;
+  text-align: center;
+  width: 250px;
+  margin-bottom: 25px;
+}
 </style>
