0531b3b6b1e524131b8fdf32f7e699fc71c7f029
Patricia Marklund <antunes@student.chalmers.se>
2022-10-12 11:23:55 +0200

    add component for Log out function

---
+++ client/src/components/LogOut.vue
@@ -0,0 +1,28 @@
+<template>
+    <b-dropdown-item href="/" @click="logout">Log out</b-dropdown-item>
+</template>
+
+<script>
+
+export default {
+  name: 'LogOut',
+  data() {
+    return {
+      toking: ''
+    }
+  },
+
+  methods: {
+    logout() {
+      this.token = ''
+      localStorage.setItem('user', this.token)
+      console.log(this.token)
+    }
+  },
+
+  created() {
+    this.token = localStorage.getItem('user')
+  }
+}
+
+</script>
